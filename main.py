ca03d3ff4536064a6da31ffccec018cfae1c386f
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2018-07-23 14:50:14 +0100

    added doc

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import numpy as np
-import os
-if os.environ.get('DISPLAY', '') == '':
-    print('Running w/o a display')
-    import matplotlib
-    matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.stats import multivariate_normal as mv
@@ -11,6 +6,12 @@ from tqdm import tqdm
 
 
 def k(x, s=1):
+    """
+    Defines a multi-dimensional Gaussian kernel
+    :param x: n points of dimension d to evaluate the kernel on, should be of hsape (n,d)
+    :param s: covariance (or variance if a scalar) of the Gaussian kernel
+    :return: evaluated kernel of shape (n,)
+    """
     if np.ndim(x) > 1:
         mu = np.zeros(x.shape[-1])
     else:
@@ -19,67 +20,92 @@ def k(x, s=1):
 
 
 def dk(x, s=1):
-    return -k(x, s) * (1 / s) * x
+    """
+    The gradient of a multi-dimensional Gaussian kernel
+    :param x: n points of dimension d to evaluate the gradient on, should be of shape (n,d)
+    :param s: covariance (or variance if a scalar)
+    :return: evaluated gradient of shape (n,d)
+    """
+    return (-1 / s) * (k(x, s) * x.T).T
 
 
 def p(x, mus, ss):
-    m = len(mus)
-    return np.sum([mv.pdf(x, mean=mus[i], cov=ss[i]) for i in range(m)], axis=0)
+    """
+    The target probability distribution. This one is a sum of Gaussian
+    :param x: n points of dimension d to evaluate the gradient on, should be of shape (n,d)
+    :param mus: means for the Gaussian
+    :param ss: covariances for the Gaussians
+    :return: evaluated distribution of shape (n,)
+    """
+    a = len(mus)
+    return np.sum([mv.pdf(x, mean=mus[i], cov=ss[i]) for i in range(a)], axis=0)
 
 
 def dp(x, mus, ss):
-    m = len(mus)
-    return np.sum([-(1 / ss[i]) * (mv.pdf(x, mean=mus[i], cov=ss[i]) * (x - mus[i]).T).T for i in range(m)], axis=0)
+    """
+    The gradient of the target probability distribution. This one is a sum of Gaussian
+    :param x: n points of dimension d to evaluate the gradient on, should be of shape (n,d)
+    :param mus: means for the Gaussian of shape (a,d)
+    :param ss: covariances for the Gaussians of shape (a,)
+    :return: evaluated gradient distribution of shape (n,d)
+    """
+    # TODO: extend to non circular covariances
+    a = len(mus)
+    return np.sum([-(1 / ss[i]) * (mv.pdf(x, mean=mus[i], cov=ss[i]) * (x - mus[i]).T).T for i in range(a)],
+                  axis=0)
 
 
-def energy(x,p,k):
+def energy(x, p, k):
+    """
+    define the energy to minimize.
+    :param x: energy evaluated at x of shape (n,d)
+    :param p: the target probability
+    :param k: the smoothing kernel
+    :return: the energy of shape (1,)
+    """
     pwx = sp.spatial.distance.pdist(x[:, None], 'euclidean')
     xz = z + x[:, np.newaxis]
     return 2/(n*(n-1))*np.sum(k(pwx, s)) - 2/(n*m)*np.sum(p(xz.flatten(), mus, ss))
 
 
-s = 3
-mus = [-4, 3]
-ss = [1, 0.5]
+if __name__ == '__main__':
 
-# start by random n points between -10 and 10
-n = 15
-x = np.random.uniform(-7, 7, n)
+    s = 2
+    mus = [-5,-1, 3]
+    ss = [1, 0.3, 0.5]
 
-# generate for each position m samples from the kernel k
-m = 100
-z = np.random.normal(0, s, (n,m))
+    # start by random n points between -10 and 10
+    n = 50
+    x = np.random.uniform(-7, 7, n)
 
-t = np.linspace(-10,10,100)
-pt = p(t, mus, ss)
+    # generate for each position m samples from the kernel k
+    m = 100
+    z = np.random.normal(0, s, (n,m))
 
-# plt.figure()
-# plt.plot(t, pt)
-# plt.scatter(x, [0]*len(x), color='green')
-# plt.title('Particles and target distribution')
-# plt.show()
+    t = np.linspace(-10, 10, 100)
+    pt = p(t, mus, ss)
 
-print(energy(x, p,k))
+    print(energy(x, p,k))
 
-lr = 1
-xnew = x.copy()
+    lr = 10
+    xnew = x.copy()
 
-plt.ion()
-fig, ax = plt.subplots(figsize=(10,6))
-ax.plot(t, pt)
-ax.scatter(xnew, [0]*len(xnew))
-plt.draw()
-
-for _ in tqdm(range(2000)):
-    for i in range(n):
-        dxi = -2/(n*m)*np.sum(dp(xnew[i]+z[i], mus, ss)) + 2/(n*(n-1))*(np.sum(dk(xnew[i] - xnew, s) - dk(0,s)))
-        xnew[i] = xnew[i] - lr*dxi
-        #print('Energy at step (%i, %i): %f' %(_,i, energy(x,p,k)))
-    del ax.collections[0]
-    ax.scatter(xnew, [0]*len(xnew), color='red')
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(10,6))
+    ax.plot(t, pt)
+    ax.scatter(xnew, [0]*len(xnew))
     plt.draw()
-    plt.pause(0.01)
 
-print(energy(xnew, p,k))
+    for _ in tqdm(range(2000)):
+        for i in range(n):
+            dxi = -2/(n*m)*np.sum(dp(xnew[i]+z[i], mus, ss)) + 2/(n*(n-1))*(np.sum(dk(xnew[i] - xnew, s) - dk(np.array([0]),s)))
+            xnew[i] = xnew[i] - lr*dxi
+            #print('Energy at step (%i, %i): %f' %(_,i, energy(x,p,k)))
+        del ax.collections[0]
+        ax.scatter(xnew, [0]*len(xnew), color='red')
+        plt.draw()
+        plt.pause(0.0001)
 
-plt.waitforbuttonpress()
+    print(energy(xnew, p,k))
+
+    plt.waitforbuttonpress()
